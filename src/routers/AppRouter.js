ad2bce95130a9eef4d8c50e31ba48df85a541dba
ianmcateer <ian-mcateer55@hotmail.com>
2018-07-16 13:59:04 +1000

    react router

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import Header from '../components/Header';
+import HomePage from '../components/HomePage';
+import CheckoutPage from '../components/CheckoutPage'
+import MenuPage from '../components/MenuPage'
+
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={HomePage} exact={true} />
+        <Route path="/menu" component={MenuPage} exact={true} />
+        <Route path="/checkout" component={CheckoutPage} exact={true} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
