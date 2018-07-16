339ef2e3fe824a9e836dafe2cdb0e5b20b6a1852
Nick Pasquariello <npasquariello@gmail.com>
2018-07-16 20:41:32 +1000

    Styled homepage and navbar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+// import Background from '../images/home-background.jpg';
 
+// var sectionStyle = {
+//   width: "100%",
+//   height: "400px",
+//   backgroundImage: `url(${Background})`
+// };
+
+// class Section extends Component {
+//   render() {
+//     return (
+//       <section style={ sectionStyle }>
+//       </section>
+//     );
+//   }
+// }
 const HomePage = () => (
-  <div>
-    <h1>Welcome</h1>
-    <p>This is my site. Take a look around!</p>
+  <div className="Homepage-background">
+    <div className="Homepage">
+    <h1>Welcome to Guzman y Gomez Kawana Catering Page</h1><br />
+    <Link to="/menu" className="MenuButton">Start your order</Link>
+    
+    </div>
   </div>
 );
 
--- src/components/MenuPage.js
+++ src/components/MenuPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const MenuPage = () => (
-  <div>
+  <div className="MenuPage">
     <h1>MenuPage</h1>
   </div>
 );
